61070a669392bcdc044ac71590714172441d22f9
Bruno <brunoterra-2011@hotmail.com>
2025-07-24 10:35:13 -0300

    Atualização

--- Js/CaixaEletronico.js
+++ Js/CaixaEletronico.js
@@ -7,22 +7,18 @@ frm.addEventListener("submit", (e) => {
 
     const valorDigitado = Number(frm.inNumero.value)
 
-    function calcularNotas(valor) {
-        let notas100 = Math.floor(valor / 100)
-        valor %= 100
-        let notas50 = Math.floor(valor / 50)
-        valor %= 50
-        let notas20 = Math.floor(valor / 20)
+    let valor = valorDigitado
+    let notas100 = Math.floor(valor / 100)
+    valor %= 100
+    let notas50 = Math.floor(valor / 50)
+    valor %= 50
+    let notas20 = Math.floor(valor / 20)
 
-        return `Notas de 100: ${notas100}\nNotas de 50: ${notas50}\nNotas de 20: ${notas20}`
-    }
-
-    const resultado = calcularNotas(valorDigitado)
+    const resultado = `Notas de 100: ${notas100}\nNotas de 50: ${notas50}\nNotas de 20: ${notas20}`
     resp.innerText = resultado
 })
 
 NovoSaque.addEventListener("click", () => {
-
     frm.inNumero.value = ""
     frm.inNumero.focus()
     resp.innerText = ""
